e5d695985a612e6232b97c8fd3878c614b221648
simplysamiul <simplysamiul@gmail.com>
2021-09-19 18:10:01 +0600

    added all code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Blogs></Blogs>
     </div>
   );
 }
 
+
+function Blogs () {
+  const [blogs, setBlogs] = useState([]);
+  useEffect( () =>{
+    fetch(`https://www.thesportsdb.com/api/v1/json/1/all_sports.php`)
+    .then(res => res.json())
+    .then(data =>setBlogs(data.sports))
+  },[])
+  return (
+    <div>
+      <h1 style = {{textAlign: 'center', backgroundColor: 'purple', padding: '20px'}}> Sports Is the Best Excersice To Your Body</h1>
+      <div className="blogStyle">
+      {
+        blogs.map(blog=> <Blog blog ={blog} key = {blog.idSport}></Blog>)
+      }
+      </div>
+    </div>
+  );
+}
+
+
+function Blog (props) {
+  const [count, setCount] = useState(0);
+  const buttonIncrease = () =>{
+    setCount(count + 1) ;
+  };
+  const buttonDecrease = () =>{
+    if(count > 0){
+      setCount(count - 1);
+    }
+  };
+  const {strSportThumb, strSport, strFormat, strSportDescription} = props.blog;
+  const description = strSportDescription.slice(0, 300);
+  return (
+      <div style = {{border: "2px solid lightgrey", backgroundColor: "lightgrey"}}>
+      <img style ={{width : "100%"}} src={strSportThumb} alt="" />
+     <div className="blog-info" style = {{padding: "10px"}}>
+     <h2>Sports : {strSport}</h2>
+      <h4>Sports Formate : {strFormat}</h4>
+      <p style = {{textAlign : "justify"}}>{description}</p>
+      <div className="button-area">
+        <button onClick = {buttonIncrease}> Increase + </button>
+      <h5>Number : {count}</h5>
+      <button onClick = {buttonDecrease}>Decrease - </button>
+      </div>
+     </div>
+      </div>
+  );
+}
 export default App;
